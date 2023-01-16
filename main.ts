7ef35117fb0ab468d792184c2e0cc2387529dc40
Tomonobu Saito <Tomonobu.Saito@gmail.com>
2023-01-16 17:53:24 +0900

    秒をつねに二けたで表示するように変更

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 function elapsed_to_text () {
-    return "" + convertToText(Math.trunc(elapsed / 60)) + ":" + convertToText(elapsed % 60)
+    return "" + convertToText(Math.trunc(elapsed / 60)) + ":" + _2digit_second()
 }
+function _2digit_second () {
+    i = elapsed % 60
+    if (i < 10) {
+        return "0" + convertToText(i)
+    } else {
+        return convertToText(i)
+    }
+}
+let i = 0
 let elapsed = 0
 let using = 0
 elapsed = 0
